bbaa9283a9f0ee68bb3127e6310a183d4c278fcc
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-29 20:48:47 +0900

    feat(last): add last function

---
+++ last.ts
@@ -0,0 +1,34 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import type { Indexable, VariableLength } from "./types.ts";
+
+/** Extract the last element of a sequence.
+ *
+ * @example
+ * ```ts
+ * import { last } from "https://deno.land/x/seqtools/last.ts";
+ * import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * assertEquals(last([1, 2, 3]), 3);
+ * assertEquals(last("abc"), "c");
+ * assertEquals(last([]), undefined);
+ * ```
+ */
+export function last<const T>(seq: readonly [...readonly unknown[], T]): T;
+export function last<const T extends string>(seq: `${T}`): LastString<T>;
+export function last<T>(
+  seq: Readonly<Indexable<T> & VariableLength>,
+): T | undefined;
+export function last<T>(
+  seq: Readonly<Indexable<T> & VariableLength>,
+): T | undefined {
+  return seq[seq.length - 1];
+}
+
+export type LastString<T extends string> = T extends "" ? undefined
+  : LastChar<T>;
+
+export type LastChar<T extends string> = T extends `${infer F}${infer Rest}`
+  ? Rest extends "" ? F : LastChar<Rest>
+  : never;
---
+++ last_test.ts
@@ -0,0 +1,69 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+
+import { last } from "./last.ts";
+import { Indexable, VariableLength } from "./types.ts";
+import {
+  assertEquals,
+  assertType,
+  describe,
+  IsExact,
+  it,
+} from "./_dev_deps.ts";
+
+describe("last", () => {
+  it("should return last of element", () => {
+    const table: [Indexable<unknown> & VariableLength, unknown][] = [
+      [[], undefined],
+      ["", undefined],
+
+      [[""], ""],
+      [["a", "b", "c"], "c"],
+      ["a", "a"],
+      ["abc", "c"],
+    ];
+
+    table.forEach(([seq, expected]) => {
+      assertEquals(last(seq), expected);
+    });
+  });
+
+  it("should infer undefined if the sequence is empty", () => {
+    const result = last([]);
+
+    assertType<IsExact<typeof result, undefined>>(true);
+
+    const result2 = last("");
+
+    assertType<IsExact<typeof result2, undefined>>(true);
+  });
+
+  it("should infer undefined or element if the sequence is array of string", () => {
+    const result = last([] as string[]);
+
+    assertType<IsExact<typeof result, undefined | string>>(true);
+
+    const result2 = last("" as string);
+
+    assertType<IsExact<typeof result2, undefined | string>>(true);
+  });
+
+  it("should infer last element if tuple", () => {
+    const result = last([""]);
+
+    assertType<IsExact<typeof result, "">>(true);
+
+    const result2 = last(["a", "b", "c"]);
+
+    assertType<IsExact<typeof result2, "c">>(true);
+  });
+
+  it("should infer last element if template string literal", () => {
+    const result = last("a");
+
+    assertType<IsExact<typeof result, "a">>(true);
+
+    const result2 = last("abc");
+
+    assertType<IsExact<typeof result2, "c">>(true);
+  });
+});

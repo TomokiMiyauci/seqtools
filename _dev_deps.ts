6bc11ea1a2c04a713000d0cbe4e4a061e14cd22d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-29 23:44:24 +0900

    chore: add dev deps

---
+++ _dev_deps.ts
@@ -0,0 +1,9 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.190.0/testing/bdd.ts";
+export {
+  assertType,
+  type IsExact,
+} from "https://deno.land/std@0.190.0/testing/types.ts";
